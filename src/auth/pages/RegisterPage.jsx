e0b99f9d48c94392bb3c27590e9329d0db5d4f45
Luis Cereño <lrcereno@falabella.cl>
2023-03-06 16:13:08 -0300

    diseño del register page

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,8 +1,60 @@
+import { Link as RouterLink } from 'react-router-dom';
+import { Google } from "@mui/icons-material"
+import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { AuthLayout } from '../layout/AuthLayout';
+
 export const RegisterPage = () => {
     return (
-        <>
-            <h1>Register page</h1>
-        </>
+        <AuthLayout title='Crear cuenta'>
+            <form>
+                <Grid container>
+
+                    <Grid item xs={ 12 } sx={{ mt: 2 }}>
+                        <TextField 
+                            label='Nombre Completo'
+                            type="text"
+                            placeholder='Nombre Completo'
+                            fullWidth
+                        />
+                    </Grid>
+
+                    <Grid item xs={ 12 } sx={{ mt: 2 }}>
+                        <TextField 
+                            label='Correo'
+                            type="email"
+                            placeholder='correo@example.com'
+                            fullWidth
+                        />
+                    </Grid>
+
+                    <Grid item xs={ 12 } sx={{ mt: 2 }}>
+                        <TextField 
+                            label='Contraseña'
+                            type="password"
+                            placeholder='Contraseña'
+                            fullWidth
+                        />
+                    </Grid>
+
+                    <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
+
+                        <Grid item xs={ 12 }>
+                            <Button variant='contained' fullWidth>
+                                Crear cuenta
+                            </Button>
+                        </Grid>
+
+                        <Grid container direction='row' justifyContent='end'>
+                            <Typography sx={{ mr: 1 }}>¿Ya tienes cuenta?</Typography>
+                            <Link component={ RouterLink } color='inherit' to='/auth/login'>
+                                Ingresar
+                            </Link>
+                        </Grid>
+
+                    </Grid>
+
+                </Grid>
+            </form>
+        </AuthLayout>
     )
 }
-
