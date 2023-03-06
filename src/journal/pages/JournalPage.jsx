6495abcd4775ec4b08103d5f066024a64fb7d524
Luis Cereño <lrcereno@falabella.cl>
2023-03-06 17:24:26 -0300

    Se crea layout para journal

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,8 +2,8 @@ import { Typography } from "@mui/material"
 
 export const JournalPage = () => {
     return (
-        <>
+        <JournalLayout>
             <Typography variant='h1'>JournalPage</Typography>
-        </>
+        </JournalLayout>
     )
 }
